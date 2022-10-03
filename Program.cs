38d01669d3557e7f0a56c1bd0f395fccf99c8afb
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-10-03 11:22:34 +0500

    task33a done

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив. Напишите программу, которая выводит
+//индексы всех элементов, равных заданному числу в массиве.
+int GetNumber(string name)
+{
+    Console.WriteLine("Введите число " + name);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(4,6);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void GetIndex(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+            Console.Write(i + " ");
+    }
+}
+int[] massiv = new int[12];
+int number = GetNumber("");
+FillArray(massiv);
+PrintArray(massiv);
+GetIndex(massiv, number);
